02b0d11a25338119bca46b6db44ea7c3803446d9
tanisha0605 <tanishakanal@gmail.com>
2024-04-08 16:36:07 +0530

    Projects section edited

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -27,16 +27,16 @@ const Projects = ({openModal,setOpenModal}) => {
             <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
           }
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+          {toggle === 'java app' ?
+            <ToggleButton active value="java app" onClick={() => setToggle('java app')}>JAVA APP'S</ToggleButton>
             :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+            <ToggleButton value="java app" onClick={() => setToggle('java app')}>JAVA APP'S</ToggleButton>
           }
           <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+          {toggle === 'aiml' ?
+            <ToggleButton active value="aiml" onClick={() => setToggle('aiml')}>AI/ML AND WEB</ToggleButton>
             :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            <ToggleButton value="aiml" onClick={() => setToggle('aiml')}>AI/ML AND WEB</ToggleButton>
           }
         </ToggleButtonGroup>
         <CardContainer>
